e170d6bb12ba8f7c8d61582894f8fdb8e70525b2
vuongpq2 <vuongpq2@vng.com.vn>
2023-11-20 14:42:57 +0700

    update user

--- shopme-webparent/shopme-backend/src/main/java/com/shopme/admin/controller/UserController.java
+++ shopme-webparent/shopme-backend/src/main/java/com/shopme/admin/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.shopme.admin.controller;
 
 import com.shopme.admin.service.IUserService;
+import com.shopme.shopmecommon.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -15,7 +19,9 @@ public class UserController {
     }
 
     @GetMapping("/users")
-    public String listAll(){
+    public String listAll(Model model){
+        List<User> users = userService.listAll();
+        model.addAttribute("listUsers", users);
         return "users";
     }
 }
